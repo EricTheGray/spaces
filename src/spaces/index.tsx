26411f01d7c7abeebb0db2e864c6a20d2e6382bc
Eric Leong <eleong95@gmail.com>
2022-08-13 20:20:09 -0700

    add prettier

--- src/spaces/index.tsx
+++ src/spaces/index.tsx
@@ -1,9 +1,7 @@
-const hi = "sdf";
+let hi = 'sdf';
 
+const tesc = [0, 1, 3, 4, 6, 4, 4];
 
 export default function SpaceProvider() {
-return (
-<h1 className="text-3xl font-bold underline">
-        Hello world!
-</h1>)
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
